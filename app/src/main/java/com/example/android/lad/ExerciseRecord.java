a21b077c5437494781223a41870c74218fa2134f
Long Nguyen <19025008@students.ltu.edu.au>
2017-09-17 23:31:26 +1000

    Database added, layout enhanced

--- app/src/main/java/com/example/android/lad/ExerciseRecord.java
+++ app/src/main/java/com/example/android/lad/ExerciseRecord.java
@@ -1,7 +1,6 @@
 package com.example.android.lad;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.Serializable;
 
 /**
  * Created by user on 29/08/2017.
@@ -9,20 +8,23 @@ import java.util.Date;
 
 // An ExerciseRecord has information about an exercise activity
 // m stands for "member"
-public class ExerciseRecord {
+public class ExerciseRecord implements Serializable{
 
+    int id;
     String mDuration;
     String mDate; // Date of exercise
     String mWeight; // Current weight on that date
     String mBodyParts;
 
 
-    /**
-     * Initialize the a new object
-     *
-     * @param mDate
-     * @param mWeight
-     */
+    public ExerciseRecord(int id, String mDuration, String mDate, String mWeight, String mBodyParts) {
+        this.id = id;
+        this.mDuration = mDuration;
+        this.mDate = mDate;
+        this.mWeight = mWeight;
+        this.mBodyParts = mBodyParts;
+    }
+
     public ExerciseRecord(String mDuration, String mDate, String mWeight, String mBodyParts) {
         this.mDuration = mDuration;
         this.mDate = mDate;
@@ -30,19 +32,21 @@ public class ExerciseRecord {
         this.mBodyParts = mBodyParts;
     }
 
-    public ExerciseRecord()
-    {
-        this(   "0.0h",
-                new SimpleDateFormat().format(new Date()),
-                "0.0kg",
-                "Body parts");
-//        Date currentDate = new Date();
-//        SimpleDateFormat dateFormater = new SimpleDateFormat("dd/MM/yyyy");
-//        String dateToString = dateFormater.format(currentDate);
+    public ExerciseRecord(){}
+//    public ExerciseRecord()
+//    {
+//        this(   "0.0h",
+//                "dd/mm/yyyy",
+//                "0.0kg",
+//                "Body parts");
+////        Date currentDate = new Date();
+////        SimpleDateFormat dateFormater = new SimpleDateFormat("dd/MM/yyyy");
+////        String dateToString = dateFormater.format(currentDate);
+//
+//
+//    }
 
-
-    }
-
+    public int getID() { return id; }
     public String getmDuration() { return mDuration; }
     public String getmDate() {
         return mDate;
@@ -52,10 +56,16 @@ public class ExerciseRecord {
     }
     public String getmBodyParts() { return mBodyParts; }
 
+    public void setID(int id) { this.id = id; }
     public void setmDuration(String mDuration) { this.mDuration = mDuration; }
     public void setmDate(String mDate) {
         this.mDate = mDate;
     }
     public void setmWeight(String mWeight) { this.mWeight = mWeight; }
     public void setmBodyParts(String mBodyParts) { this.mBodyParts = mBodyParts;}
+
+    public String toString()
+    {
+        return "" + id + mDuration + mDate + mWeight + mBodyParts;
+    }
 }
--- app/src/main/java/com/example/android/lad/MainActivity.java
+++ app/src/main/java/com/example/android/lad/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,38 +23,61 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     FloatingActionButton fab;
     Date currentDate;
+    ListView recordsListView;
+    ArrayList<ExerciseRecord> records;
+    ArrayList<ExerciseRecord> exerciseRecords;
+    DatabaseHandler database;
+    ExerciseRecordAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        getApplicationContext().deleteDatabase("exercisesManager");
+        // Get Database
+        database = new DatabaseHandler(getApplicationContext());
+
+        // Use ArrayList to hold record data
+        exerciseRecords = new ArrayList<ExerciseRecord>();
+
+        // Load Database
+        loadDatabase();
+
+        // Delete all records
+//        deleteAllRecords();
+
 
         // Set toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         myToolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(myToolbar);
 
-        // Use ArrayList to hold record data
-        final ArrayList<ExerciseRecord> exerciseRecords = new ArrayList<>();
-
         // Add test data
         //exerciseRecords.add(new ExerciseRecord("", ""));
 
         // Find Record ListView in the layout
-        ListView recordsListView = (ListView) findViewById(R.id.records_list_view);
+        recordsListView = (ListView) findViewById(R.id.records_list_view);
 
         // Create adapter for ListView
-        final ExerciseRecordAdapter adapter = new ExerciseRecordAdapter(this, exerciseRecords);
+        adapter = new ExerciseRecordAdapter(this, exerciseRecords);
+
+        //adapter.setNotifyOnChange(true);
 
         // Set Adapter on ListView so the list item will be automatically created
         recordsListView.setAdapter(adapter);
 
         recordsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 Toast.makeText(MainActivity.this, "Details of record", Toast.LENGTH_SHORT).show();
-                Intent intent_calendar = new Intent(MainActivity.this, RecordDetailsActivity.class);
-                startActivity(intent_calendar);
+
+                ExerciseRecord r = exerciseRecords.get(position);
+
+                Intent detailsIntent = new Intent(MainActivity.this, RecordDetailsActivity.class);
+
+                detailsIntent.putExtra("record", r);
+
+                startActivity(detailsIntent);
             }
         });
 
@@ -61,12 +85,72 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentDate = new Date();
+                Toast.makeText(getApplicationContext(), "Adding record", Toast.LENGTH_SHORT).show();
+//                exerciseRecords.add(new ExerciseRecord("0.0h", "dd/mm/yyyy", "0.0kg", ""));
+//                Log.d("Insert: ", "" + exerciseRecords.size());
+
+//                DatabaseHandler database = new DatabaseHandler(getApplicationContext());
+                // Inserting record
+                Date currentDate = new Date();
                 SimpleDateFormat dateFormater = new SimpleDateFormat("dd/MM/yyyy");
                 String dateToString = dateFormater.format(currentDate);
-                Toast.makeText(getApplicationContext(), "Adding record", Toast.LENGTH_SHORT).show();
-                exerciseRecords.add(new ExerciseRecord());
+                Log.d("Insert: ", "Inserting ..");
+                database.addRecord(new ExerciseRecord("0.0", dateToString, "0.0", "Body parts"));
+                Log.d("Insert: ", "" + exerciseRecords.size());
+                loadDatabase();
+                Log.d("Insert: ", "" + exerciseRecords.size());
+
+                //exerciseRecords.add(new ExerciseRecord(100, "a", "b", "c", "d"));
                 adapter.notifyDataSetChanged();
+
+                Log.d("Insert: ", "Notify adapter ...");
+//                // Reading all record
+//                Log.d("Reading: ", "Reading all records ...");
+//                exerciseRecords = database.getAllRecords();
+//                for (ExerciseRecord r : records) {
+//                    String log = "Id: "+ r.getID()
+//                            + " ,Duration: " + r.getmDuration()
+//                            + " ,Date: " + r.getmDate()
+//                            + " ,Weight: " + r.getmWeight();
+//                    // Writing Contacts to log
+//                    Log.d("Name: ", log);
+//                }
+//
+//                Log.d("Deleting: ", "Deleting all records ...");
+//                for (ExerciseRecord r : exerciseRecords){
+//                    database.deleteRecord(r);
+//                }
+
+//                DBHandler db = new DBHandler(getApplicationContext());
+//                // Inserting students
+//                Log.d("Insert: ", "Inserting ..");
+//                db.addStudent(new Student("Mat", "43540"));
+//
+//                // Reading all students
+//                Log.d("Reading: ", "Reading all students..");
+//                List<Student> students = db.getAllStudents();
+//                for (Student cn : students) {
+//                    String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: "
+//                            + cn.getPhone();
+//                    // Writing Contacts to log
+//                    Log.d("Name: ", log);
+//                }
+//
+//                Log.d("Deleting: ", "Deleting all students..");
+//                for (Student cn : students){
+//                    db.deleteStudent(cn);
+//                }
+//
+//                getApplicationContext().deleteDatabase("studentsManager");
+//                String[] dbs = getApplicationContext().databaseList();
+//                try
+//                {
+//                    dbs[0].isEmpty();
+//                }
+//                catch (Exception e){
+//                    Log.d("list db: ", "No db");
+//                }
+
             }
         });
     }
@@ -116,4 +200,37 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    public void loadDatabase()
+    {
+        Log.d("Reading: ", "Reading all records ...");
+        exerciseRecords.clear();
+        records = database.getAllRecords();
+        exerciseRecords.addAll(records);
+        for (ExerciseRecord r : exerciseRecords) {
+            String log = "Id: "+ r.getID()
+                    + ", Duration: " + r.getmDuration()
+                    + ", Date: " + r.getmDate()
+                    + ", Weight: " + r.getmDate()
+                    + ", Body parts: " + r.getmBodyParts();
+            // Writing Contacts to log
+            Log.d("Name: ", log);
+        }
+    }
+
+    public void deleteAllRecords()
+    {
+        Log.d("Deleting: ", "Deleting all records ...");
+            for (ExerciseRecord r : exerciseRecords){
+                database.deleteRecord(r);
+            }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadDatabase();
+        adapter.notifyDataSetChanged();
+    }
+
 }
